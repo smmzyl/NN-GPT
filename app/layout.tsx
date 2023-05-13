430fc1dab2b6858a714f9493dd1cc6ce604c337d
zhangyiliang <zhangyiliang@highsai.com>
2023-05-13 15:06:09 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "nn-gpt",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "nn-gpt",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
