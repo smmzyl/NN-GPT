d7c88c2610296fd32ecca6a83a45417447ab1bd6
zhangyiliang <zhangyiliang@highsai.com>
2023-05-14 16:29:39 +0800

    bug fix

--- app/constant.ts
+++ app/constant.ts
@@ -1,11 +1,11 @@
-// export const OWNER = "kimi";
-// export const REPO = "NN-GPT";
-// export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
-// export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
-// export const UPDATE_URL = `${REPO_URL}#keep-updated`;
-// export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
-// export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
-// export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
+export const OWNER = "kimi";
+export const REPO = "NN-GPT";
+export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
+export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
+export const UPDATE_URL = `${REPO_URL}#keep-updated`;
+export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
+export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
+export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
 
 export enum Path {
   Home = "/",
