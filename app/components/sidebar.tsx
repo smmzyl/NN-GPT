9988ff402cca4199ff4f141d991141711175472f
zhangyiliang <zhangyiliang@highsai.com>
2023-05-14 16:20:15 +0800

    test

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,7 +119,7 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>NN GPT</div>
+        <div className={styles["sidebar-title"]}>NN-GPT</div>
         <div className={styles["sidebar-sub-title"]}>
           Build your own nn assistant.
         </div>
